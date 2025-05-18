8f0c64a9d38205a32c7b057ef7b7ee79205a8142
kotatumikan <1471033@gmail.com>
2025-05-18 16:15:40 +0900

    firstCommit

---
+++ src/main/java/com/example/demo/SpringTeamBlueApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTeamBlueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTeamBlueApplication.class, args);
+	}
+
+}
