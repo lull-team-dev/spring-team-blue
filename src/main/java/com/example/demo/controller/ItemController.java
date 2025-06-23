6906c7085cf9fa8efd59686d43f031db5b19fc5a
kotatumikan <1471033@gmail.com>
2025-06-24 06:51:35 +0900

    商品絞り込み完了。残りHTML表示

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,11 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.repository.CategoryRepository;
+import com.example.demo.service.ItemService;
 
 @Controller
 public class ItemController {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	@Autowired
+	ItemService itemService;
 
 	// 商品登録フォーム
 	@GetMapping("items/new")
@@ -27,7 +39,8 @@ public class ItemController {
 
 	// 商品一覧
 	@GetMapping({ "item/item_list", "/" })
-	public String index() {
+	public String index(@RequestParam(value = "categoryId", required = false) Integer categoryId, Model model) {
+		itemService.loadItemPage(categoryId, model);
 		return "item/item_list";
 	}
 
