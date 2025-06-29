769108a1f08315078e2ce61b31a469238c32f3ea
kotatumikan <1471033@gmail.com>
2025-06-29 21:45:54 +0900

    Revert "Merge pull request #13 from lull-team-dev/feature/item-detail"
    This reverts commit a1d9bba2a7b045a058d763c4ee1313d8b16bfec8, reversing
    changes made to 474e4880d9b55a7dba6704f7c718a190a7ca6273.

--- src/main/java/com/example/demo/entity/Account.java
+++ src/main/java/com/example/demo/entity/Account.java
@@ -1,31 +1,14 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users")
 public class Account {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 
-	private String name;
-
-	public Integer getId() {
-		return id;
+	protected Account() {
 	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
 }
--- src/main/java/com/example/demo/entity/Item.java
+++ src/main/java/com/example/demo/entity/Item.java
@@ -1,109 +1,14 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "items")
 public class Item {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id; // 商品ID
+	private Long id;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "user_id", nullable = false) // ユーザー（出品者）を参照
-	private Account account;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "category_id", nullable = false) // カテゴリーを参照
-	private Category category;
-
-	@Column(nullable = false)
-	private String name; // 商品名
-
-	@Column(nullable = false)
-	private String image; // 画像ファイル名
-
-	@Column(nullable = false)
-	private String memo; // 商品説明
-
-	@Column(nullable = false)
-	private Integer price; // 価格
-
-	@Column(name = "sold_out", nullable = false)
-	private Boolean soldOut; // 売り切れかどうか
-
-	// --- Getter & Setter ---
-
-	public Integer getId() {
-		return id;
-	}
-
-	public Account getAccount() {
-		return account;
-	}
-
-	public Category getCategory() {
-		return category;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public String getMemo() {
-		return memo;
-	}
-
-	public Integer getPrice() {
-		return price;
-	}
-
-	public Boolean getSoldOut() {
-		return soldOut;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public void setAccount(Account account) {
-		this.account = account;
-	}
-
-	public void setCategory(Category category) {
-		this.category = category;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-
-	public void setSoldOut(Boolean soldOut) {
-		this.soldOut = soldOut;
+	protected Item() {
 	}
 }
