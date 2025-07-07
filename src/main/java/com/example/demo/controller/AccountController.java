c1a6ae6fdd85d588ab7e14ecdf94026d01d1b017
kotatumikan <1471033@gmail.com>
2025-07-08 06:55:49 +0900

    動作チェック完了。このブランチをdevに反映させたい

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -177,9 +176,4 @@ public class AccountController {
 		return "redirect:/mypage";
 	}
 
-	// ユーザー詳細
-	@GetMapping("/user/{id}/detail")
-	public String showUserDetail(@PathVariable("id") Long id) {
-		return "user/user_detail";
-	}
 }
