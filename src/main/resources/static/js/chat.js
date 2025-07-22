c9fc2b5a0963f438f132bb4c95d2699053d705c4
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-07-22 09:33:28 +0900

    style: チャット画面の吹き出し追加とリアルタイムメッセージ部分のレイアウト調整

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -17,29 +17,23 @@ function setConnected(connected) {
 
 // 接続を行う。
 function connect() {
-  var socket = new SockJS('/chat'); // WebSocket通信開始
+  var socket = new SockJS("/chat"); // WebSocket通信開始
   stompClient = Stomp.over(socket);
   stompClient.connect({}, function (frame) {
     setConnected(true);
-    console.log('Connected: ' + frame);
+    console.log("Connected: " + frame);
     // /receive/messageエンドポイントでメッセージを受信し、表示する
-    stompClient.subscribe('/receive/message', function (response) {
+    stompClient.subscribe("/receive/message", function (response) {
       showMessage(JSON.parse(response.body));
     });
   });
 }
 
 $(document).ready(function () {
-	
-	setTimeout(() => {
-	  connect();
-	}, 3000);
-	
+  setTimeout(() => {
+    connect();
+  }, 3000);
 });
-
-
-
-
 
 // 接続の切断
 function disconnect() {
@@ -52,17 +46,20 @@ function disconnect() {
 
 // メッセージの送信
 function sendMessage() {
-   //send/messageエンドポイントにメッセージを送信する
-  stompClient.send("/send/message", {}, JSON.stringify(
-   {'name': $("#name").val(), 
-	'statement': $("#statement").val(),
-	'itemId':$("#itemId").val(),
-	'chatId':$("#chatId").val(),
-	'myAccountId':$("#myAccountId").val(),
+  //send/messageエンドポイントにメッセージを送信する
+  stompClient.send(
+    "/send/message",
+    {},
+    JSON.stringify({
+      name: $("#name").val(),
+      statement: $("#statement").val(),
+      itemId: $("#itemId").val(),
+      chatId: $("#chatId").val(),
+      myAccountId: $("#myAccountId").val(),
+    })
+  );
+  $("#statement").val("");
 
-	}));
-  $("#statement").val('');
-  
   //stompClient.send("/send/message", {
   //  "content-type": "application/json"
   //}, JSON.stringify({
@@ -81,34 +78,75 @@ function sendMessage() {
 //      "<tr><td>" + message.name + ": " + message.statement + "</td></tr>");
 //}
 
+// function showMessage(message) {
+//   console.log("受信:", message);
+//   $("#message").append(
+//     "<tr><td>" + $('<div/>').text(message.name).html() + ": " + $('<div/>').text(message.statement).html() + "</td></tr>");
+// }
 
 function showMessage(message) {
-  console.log("受信:", message);
-  $("#message").append(
-    "<tr><td>" + $('<div/>').text(message.name).html() + ": " + $('<div/>').text(message.statement).html() + "</td></tr>");
+  const myId = $("#myAccountId").val();
+  const isMine = String(message.myAccountId) === String(myId);
+
+  const bgColor = isMine ? "bg-[#BAEFFF]" : "bg-[#ECFEFE]";
+  const justify = isMine ? "justify-end" : "justify-start";
+
+  const triangle = isMine
+    ? `
+      <span class="absolute bottom-2 -right-2
+                   w-0 h-0
+                   border-l-[10px] border-l-[#ccc]
+                   border-t-[8px] border-t-transparent
+                   border-b-[8px] border-b-transparent"></span>
+      <span class="absolute bottom-2 -right-[7px]
+                   w-0 h-0
+                   border-l-[10px] border-l-[#BAEFFF]
+                   border-t-[8px] border-t-transparent
+                   border-b-[8px] border-b-transparent"></span>
+    `
+    : `
+      <span class="absolute bottom-2 -left-2
+                   w-0 h-0
+                   border-r-[10px] border-r-[#ccc]
+                   border-t-[8px] border-t-transparent
+                   border-b-[8px] border-b-transparent"></span>
+      <span class="absolute bottom-2 -left-[7px]
+                   w-0 h-0
+                   border-r-[10px] border-r-[#ECFEFE]
+                   border-t-[8px] border-t-transparent
+                   border-b-[8px] border-b-transparent"></span>
+    `;
+
+  $("#message").append(`
+    <div class="flex ${justify} mb-2">
+      <div class="relative inline-block min-w-[300px] max-w-[80%] p-3 rounded-xl border border-[#ccc] shadow-sm ${bgColor}">
+        <div class="text-sm text-gray-600 mb-1">${$("<div/>")
+          .text(message.name)
+          .html()}</div>
+        <div class="whitespace-pre-wrap">${$("<div/>")
+          .text(message.statement)
+          .html()}</div>
+        ${triangle}
+      </div>
+    </div>
+  `);
+
+  const messageArea = document.getElementById("message");
+  messageArea.scrollTop = messageArea.scrollHeight;
 }
 
-
-
 // windows.onloadの処理
-$(function () {  
-	
-	$(".form-inline").on('submit', function (e) {
-	  e.preventDefault();
-	  sendMessage();
-	});
+$(function () {
+  $(".form-inline").on("submit", function (e) {
+    e.preventDefault();
+    sendMessage();
+  });
 
-	$("#disconnect").click(function () {
-	  disconnect();
-	});
-	
-//	$("#send").click(function () {
-//	  sendMessage();
-//	});
+  $("#disconnect").click(function () {
+    disconnect();
+  });
 
+  //	$("#send").click(function () {
+  //	  sendMessage();
+  //	});
 });
-
-
-
-
-
